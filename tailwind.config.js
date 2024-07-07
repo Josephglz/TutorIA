8be7473acf16a698c8a20d53135d4a19bd23bb7e
Josephglz <joseph220199@gmail.com>
2024-07-06 19:36:48 -0600

    feat: Add Generative AI library
    Adds the `@google/generative-ai` package for using Google's generative AI models.
    
    This change introduces a new dependency to integrate Google's generative AI models into the application. This library will be used to generate creative content such as text, images, and more, enhancing the capabilities of the application.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-background': '#F0F0F0'
+      }
+    },
   },
   plugins: [],
 }
