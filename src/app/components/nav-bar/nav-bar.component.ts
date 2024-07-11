5cb782eb78484e3faa2cb62a5233afc80018e73f
Josephglz <joseph220199@gmail.com>
2024-07-10 18:47:16 -0600

    feat: Implement navbar component
    Adds a navbar component with user profile information and a dropdown menu for user actions.
    This component utilizes data from the AppService and presents a dynamic user interface.
    The dropdown menu allows the user to update their data and log out.

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { AppService } from '../../services/app.service';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+  shortFullName: string = '';
+  shortName: string = '';
+  email: string = '';
+  gender: boolean = false;
+  collapseOptions: boolean = true;
+
+  constructor(private _appService: AppService) { }
+
+  ngOnInit(): void {
+    this.shortFullName = this._appService.getShortFullName();
+    this.shortName = this._appService.getShortName();
+    this.email = this._appService.StudentData.email;
+    this.gender = this._appService.StudentData.gender;
+  }
+
+  toggleOptions(): void {
+    this.collapseOptions = !this.collapseOptions;
+  }
+}
